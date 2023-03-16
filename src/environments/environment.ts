9f4890f501044cd25b3ceec3d75c33b81977cc37
Canh Nguyen <canh.nguyen@emandai.net>
2023-03-16 08:38:43 +0700

    [Main] Implement enviroment

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA86q4HYM4UA03kIcYgbvIaxAx2cGXTEkc',
+    authDomain: 'ngxtest17337.firebaseapp.com',
+    projectId: 'ngxtest17337',
+    storageBucket: 'ngxtest17337.appspot.com',
+    messagingSenderId: '306760538894',
+    appId: '1:306760538894:web:fce64e3fdea17c4699155f',
+    measurementId: 'G-6QDCFQVFV4',
+  },
+};
